cae42454e13a98bef2d409cdcd0c253f095edb35
kodics <kodicsworld@gmail.com>
2023-12-30 20:17:58 -0800

    airbnb

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-"""starts a Flask web application
-"""
+""" Script that runs an app with Flask framework """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """closes
-    """
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
 
-@app.route('/states')
-def states():
-    """display a HTML page: (inside the tag BODY)
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
+@app.route('/states/', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def display_html(id=None):
+    """ Function called with /states route """
+    states = storage.all(State)
 
+    if not id:
+        dict_to_html = {value.id: value.name for value in states.values()}
+        return render_template('7-states_list.html',
+                               Table="States",
+                               items=dict_to_html)
 
-@app.route('/states/<id>')
-def cities_by_states(id):
-    """display a HTML page: (inside the tag BODY)
-    """
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+    k = "State.{}".format(id)
+    if k in states:
+        return render_template('9-states.html',
+                               Table="State: {}".format(states[k].name),
+                               items=states[k])
 
+    return render_template('9-states.html',
+                           items=None)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
