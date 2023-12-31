e39f0af3a7911419babce8f1c4eb394c580be8bc
kodics <kodicsworld@gmail.com>
2023-12-30 19:58:13 -0800

    airbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,21 @@
- #!/usr/bin/python3
-"""starts a Flask web application
-"""
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
 from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    """prints "Hello HBNB!"
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function called with / route """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """prints “HBNB”
-    """
+    """ Function called with /hbnb route """
     return 'HBNB'
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
